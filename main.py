8a611519e06e09760403666dc88c69a222aee30b
Ricky Renaldi <145863352+rey1711@users.noreply.github.com>
2025-05-16 22:01:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="API Prediksi Penyusutan Aset Masa Depan")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
